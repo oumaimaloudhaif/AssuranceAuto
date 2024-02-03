dd21d1862a45e6d5b68c0907e1f6db34c1de2b86
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-02-03 23:08:38 +0100

    feat: Implement mapper logic for data transformation

--- src/main/java/com/example/AssuranceAuto/dtos/mappers/FromDTOToDO.java
+++ src/main/java/com/example/AssuranceAuto/dtos/mappers/FromDTOToDO.java
@@ -1,2 +1,22 @@
-package com.example.AssuranceAuto.dtos.mappers;public class FromDtoToDo {
+package com.example.AssuranceAuto.dtos.mappers;
+
+import com.example.AssuranceAuto.dtos.AssuranceDTO;
+import com.example.AssuranceAuto.dtos.AutoDTO;
+import com.example.AssuranceAuto.entities.Assurance;
+import com.example.AssuranceAuto.entities.Auto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FromDTOToDO {
+    public Auto MapAutoDTO(AutoDTO autoDto) {
+        Auto auto = new Auto();
+        auto.setModel(autoDto.getModel());
+        auto.setRegistrationNumber(autoDto.getRegistrationNumber());
+        return auto;
+    }
+    public Assurance MapAssuranceDTO(AssuranceDTO assuranceDTO) {
+        Assurance assurance = new Assurance();
+        assurance.setAssuranceNumber(assuranceDTO.getAssuranceNumber());
+        return assurance;
+    }
 }
--- src/main/java/com/example/AssuranceAuto/dtos/mappers/FromDoToDTO.java
+++ src/main/java/com/example/AssuranceAuto/dtos/mappers/FromDoToDTO.java
@@ -1,2 +1,22 @@
-package com.example.AssuranceAuto.dtos.mappers;public class FromDoToDto {
+package com.example.AssuranceAuto.dtos.mappers;
+
+import com.example.AssuranceAuto.dtos.AssuranceDTO;
+import com.example.AssuranceAuto.dtos.AutoDTO;
+import com.example.AssuranceAuto.entities.Assurance;
+import com.example.AssuranceAuto.entities.Auto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FromDoToDTO {
+    public AutoDTO MapAuto(Auto auto) {
+        AutoDTO autoDTO = new AutoDTO();
+        autoDTO.setModel(auto.getModel());
+        autoDTO.setRegistrationNumber(auto.getRegistrationNumber());
+        return autoDTO;
+    }
+    public AssuranceDTO MapAssurance(Assurance assurance) {
+        AssuranceDTO assuranceDTO = new AssuranceDTO();
+        assuranceDTO.setAssuranceNumber(assurance.getAssuranceNumber());
+        return assuranceDTO;
+    }
 }
