bf2a7a28e30548dbfa32f6255c4eab056d61c4ac
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-02-03 23:10:58 +0100

    feat: Implement repositories for data management

--- src/main/java/com/example/AssuranceAuto/repositories/AssuranceRepository.java
+++ src/main/java/com/example/AssuranceAuto/repositories/AssuranceRepository.java
@@ -1,2 +1,13 @@
-package com.example.AssuranceAuto.repositories;public interface AssuranceRepository {
+package com.example.AssuranceAuto.repositories;
+
+import com.example.AssuranceAuto.entities.Assurance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/*** Assurance Repository ***/
+@Repository
+public interface AssuranceRepository extends JpaRepository<Assurance,Long> {
+    List<Assurance> findByAssuranceNumber(String assuranceNumber);
 }
