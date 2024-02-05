aa06a87ac157672641461acc2f33e227db2da8b5
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-02-05 11:29:51 +0100

    feat: Create AssuranceController and AutoController

--- src/main/java/com/example/AssuranceAuto/controllers/AssuranceController.java
+++ src/main/java/com/example/AssuranceAuto/controllers/AssuranceController.java
@@ -1,2 +1,40 @@
-package com.example.AssuranceAuto.controllers;public class AssuranceController {
+package com.example.AssuranceAuto.controllers;
+
+import com.example.AssuranceAuto.controllers.mappers.AssuranceMapper;
+import com.example.AssuranceAuto.controllers.requests.AssuranceRequest;
+import com.example.AssuranceAuto.controllers.responses.AssuranceResponse;
+import com.example.AssuranceAuto.dtos.AssuranceDTO;
+import com.example.AssuranceAuto.entities.Assurance;
+import com.example.AssuranceAuto.servicesImpl.AssuranceServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+/*** Assurance Controller ***/
+@Validated
+@RestController
+public class AssuranceController {
+    @Autowired  AssuranceServiceImpl assuranceServiceImpl;
+    @Autowired  AssuranceMapper assuranceMapper;
+    @GetMapping("/assurances")
+    public AssuranceResponse getAssurances(@RequestBody (required = false) AssuranceRequest assuranceRequest){
+        List<AssuranceDTO> assuranceDTOS;
+        if(assuranceRequest!=null && !(assuranceRequest.getKeyword().isBlank()))
+            assuranceDTOS=assuranceServiceImpl.searchAssuranceByAssuranceNumber(assuranceRequest.getKeyword());
+        else
+            assuranceDTOS=assuranceServiceImpl.getAllAssurances();
+        return assuranceMapper.fromAssuranceDTOToAssurance(assuranceDTOS);
+    }
+    @PostMapping("/assurances")
+    public AssuranceDTO addAssurance(@RequestBody @Valid Assurance assurance){
+       return  assuranceServiceImpl.addAssurance(assurance);
+    }
+    @PutMapping("/assurances")
+    public AssuranceDTO updateAssurance(@RequestBody @Valid Assurance assurance){
+        return  assuranceServiceImpl.updateAuto(assurance);
+    }
 }
--- src/main/java/com/example/AssuranceAuto/controllers/AutoController.java
+++ src/main/java/com/example/AssuranceAuto/controllers/AutoController.java
@@ -1,2 +1,39 @@
-package com.example.AssuranceAuto.controllers;public class AutoController {
+package com.example.AssuranceAuto.controllers;
+
+import com.example.AssuranceAuto.controllers.mappers.AutoMapper;
+import com.example.AssuranceAuto.controllers.requests.AutoRequest;
+import com.example.AssuranceAuto.controllers.responses.AutoResponse;
+import com.example.AssuranceAuto.dtos.AutoDTO;
+import com.example.AssuranceAuto.entities.Auto;
+import com.example.AssuranceAuto.servicesImpl.AutoServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*** Auto Controller ***/
+@Validated
+@RestController
+public class AutoController {
+    @Autowired AutoServiceImpl autoServiceImpl;
+    @Autowired AutoMapper autoMapper;
+    @GetMapping("/autos")
+    public AutoResponse getAutos(@RequestBody (required = false) AutoRequest autoRequest){
+        List<AutoDTO> autoDTOS;
+        if(autoRequest!=null && !(autoRequest.getKeyword().isBlank()))
+            autoDTOS=autoServiceImpl.searchRegistrationNumber(autoRequest.getKeyword());
+        else
+            autoDTOS=autoServiceImpl.getAllAutos();
+        return autoMapper.fromAutoDTOToAuto(autoDTOS);
+    }
+    @PostMapping("/autos")
+    public AutoDTO addAuto(@RequestBody @Valid Auto auto){
+        return autoServiceImpl.addAuto(auto);
+    }
+    @PutMapping("/autos")
+    public AutoDTO updateAuto(@RequestBody @Valid Auto auto){
+        return autoServiceImpl.updateAuto(auto);
+    }
 }
