fd06e0aa0cdad2a9af83e2fef679f9fc796dd99a
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-02-23 23:00:11 +0100

    refactor: Enhance Controllers

--- src/main/java/com/example/AssuranceAuto/controllers/AssuranceController.java
+++ src/main/java/com/example/AssuranceAuto/controllers/AssuranceController.java
@@ -10,7 +10,13 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 /** Assurance Controller */
 @Validated
@@ -24,8 +30,7 @@ public class AssuranceController {
       @RequestBody(required = false) @Valid AssuranceRequest assuranceRequest) {
     List<AssuranceDTO> assuranceDTOS;
     if (assuranceRequest != null && !(assuranceRequest.getKeyword().isBlank())) {
-      assuranceDTOS =
-          assuranceServiceImpl.searchAssuranceByAssuranceNumber(assuranceRequest.getKeyword());
+      assuranceDTOS = assuranceServiceImpl.searchAssuranceByAssuranceNumber(assuranceRequest.getKeyword());
     } else {
       assuranceDTOS = assuranceServiceImpl.getAllAssurances();
     }
