415c861b844902d6c1aa029059666da8a904d985
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-02-23 22:46:35 +0100

    refactor: Enhance services and services implementations

--- src/main/java/com/example/AssuranceAuto/servicesImpl/AssuranceServiceImpl.java
+++ src/main/java/com/example/AssuranceAuto/servicesImpl/AssuranceServiceImpl.java
@@ -16,39 +16,96 @@ public class AssuranceServiceImpl implements AssuranceService {
   @Autowired private AssuranceRepository assuranceRepository;
   @Autowired private FromDOToDTO fromDoToDTO;
 
+  /**
+   * @param assurance the assurance object to be added
+   * @return AssuranceDTO
+  */
+  @Override
   public AssuranceDTO addAssurance(Assurance assurance) {
     final Assurance savedAssurance = assuranceRepository.save(assurance);
 
-    return fromDoToDTO.MapAssurance(savedAssurance);
+    return fromDoToDTO.mapAssurance(savedAssurance);
   }
 
+  /**
+   * @param assurance the assurance object to be updated
+   * @return AssuranceDTO
+  */
+  @Override
   public AssuranceDTO updateAssurance(Assurance assurance) {
     final Assurance updatedAssurance = assuranceRepository.save(assurance);
 
-    return fromDoToDTO.MapAssurance(updatedAssurance);
+    return fromDoToDTO.mapAssurance(updatedAssurance);
   }
 
+  /**
+   *
+   * @return List<AssuranceDTO>
+  */
   public List<AssuranceDTO> getAllAssurances() {
     List<Assurance> assurances = assuranceRepository.findAll();
     List<AssuranceDTO> assuranceDTOs = new ArrayList<>();
     assurances.forEach(
         assurance -> {
-          AssuranceDTO assurancesDTO = fromDoToDTO.MapAssurance(assurance);
+          AssuranceDTO assurancesDTO = fromDoToDTO.mapAssurance(assurance);
           assuranceDTOs.add(assurancesDTO);
         });
 
     return assuranceDTOs;
   }
 
+  /**
+   * @param assuranceNumber to search for assurances
+   * @return List<AssuranceDTO>
+  */
+  @Override
   public List<AssuranceDTO> searchAssuranceByAssuranceNumber(String assuranceNumber) {
     final List<Assurance> assurances = assuranceRepository.findByAssuranceNumber(assuranceNumber);
     List<AssuranceDTO> assuranceDTOS = new ArrayList<>();
     assurances.forEach(
         assurance -> {
-          AssuranceDTO assuranceDTO = fromDoToDTO.MapAssurance(assurance);
+          AssuranceDTO assuranceDTO = fromDoToDTO.mapAssurance(assurance);
           assuranceDTOS.add(assuranceDTO);
         });
 
     return assuranceDTOS;
   }
+
+    /**
+     * Retrieves an assurance by its ID.
+     *
+     * @param assuranceId the ID of the assurance to retrieve
+     * @return the AssuranceDTO corresponding to the assurance, or null if the assurance does not exist
+     */
+
+    @Override
+    public AssuranceDTO getAssuranceById(Long assuranceId) {
+        final Assurance assurance = assuranceRepository.findById(assuranceId).orElse(null);
+        if (assurance != null) {
+
+            return fromDoToDTO.mapAssurance(assurance);
+        } else {
+
+            return null;
+        }
+    }
+
+    /**
+     * Deletes an assurance by its ID.
+     *
+     * @param assuranceId the ID of the assurance to delete
+     * @return true if the assurance was deleted successfully, false otherwise
+     */
+    @Override
+    public boolean deleteAssuranceById(Long assuranceId) {
+        final Assurance assurance = assuranceRepository.findById(assuranceId).orElse(null);
+        if (assurance != null) {
+            assuranceRepository.delete(assurance);
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
 }
--- src/main/java/com/example/AssuranceAuto/servicesImpl/AutoServiceImpl.java
+++ src/main/java/com/example/AssuranceAuto/servicesImpl/AutoServiceImpl.java
@@ -16,36 +16,52 @@ public class AutoServiceImpl implements AutoService {
   @Autowired private AutoRepository autoRepository;
   @Autowired private FromDOToDTO fromDoToDTO;
 
+  /**
+  * @param auto the auto object to be added
+  * @return AutoDTO
+   */
   public AutoDTO addAuto(Auto auto) {
     final Auto savedAuto = autoRepository.save(auto);
 
-    return fromDoToDTO.MapAuto(savedAuto);
+    return fromDoToDTO.mapAuto(savedAuto);
   }
 
+  /**
+  * @param auto the auto object to be updated
+  * @return AutoDTO
+  */
   public AutoDTO updateAuto(Auto auto) {
     final Auto updateAuto = autoRepository.save(auto);
 
-    return fromDoToDTO.MapAuto(updateAuto);
+    return fromDoToDTO.mapAuto(updateAuto);
   }
 
+  /**
+  *
+  * @return List<AutoDTO>
+  */
   public List<AutoDTO> getAllAutos() {
     List<Auto> autos = autoRepository.findAll();
     List<AutoDTO> autosDTOs = new ArrayList<>();
     autos.forEach(
         auto -> {
-          AutoDTO autoDTO = fromDoToDTO.MapAuto(auto);
+          AutoDTO autoDTO = fromDoToDTO.mapAuto(auto);
           autosDTOs.add(autoDTO);
         });
 
     return autosDTOs;
   }
 
+  /**
+   * @param model to search for autos
+   * @return List<AutoDTO>
+  */
   public List<AutoDTO> searchAutoByModel(String model) {
     List<Auto> autos = autoRepository.findByModel(model);
     List<AutoDTO> autosDTOs = new ArrayList<>();
     autos.forEach(
         auto -> {
-          AutoDTO autoDTO = fromDoToDTO.MapAuto(auto);
+          AutoDTO autoDTO = fromDoToDTO.mapAuto(auto);
           autosDTOs.add(autoDTO);
         });
 
@@ -57,10 +73,48 @@ public class AutoServiceImpl implements AutoService {
     List<AutoDTO> autosDTOs = new ArrayList<>();
     autos.forEach(
         auto -> {
-          AutoDTO autoDTO = fromDoToDTO.MapAuto(auto);
+          AutoDTO autoDTO = fromDoToDTO.mapAuto(auto);
           autosDTOs.add(autoDTO);
         });
 
     return autosDTOs;
   }
+
+    /**
+     * Retrieves an assurance by its ID.
+     *
+     * @param autoId the ID of the auto to retrieve
+     * @return the auto corresponding to the auto, or null if the auto does not exist
+     */
+
+    @Override
+    public AutoDTO getAutoById(Long autoId) {
+        final Auto auto = autoRepository.findById(autoId).orElse(null);
+        if (auto != null) {
+
+            return fromDoToDTO.mapAuto(auto);
+        } else {
+
+            return null;
+        }
+    }
+
+    /**
+     * Deletes an auto by its ID.
+     *
+     * @param autoId the ID of the auto to delete
+     * @return true if the auto was deleted successfully, false otherwise
+     */
+    @Override
+    public boolean deleteAutoById(Long autoId) {
+        final Auto auto = autoRepository.findById(autoId).orElse(null);
+        if (auto != null) {
+            autoRepository.delete(auto);
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
 }
